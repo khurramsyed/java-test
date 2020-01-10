adbd9f712c0b6c4ced987195d103ef30d0629cb5
khurram <mahmood.khurram@gmail.com>
2020-01-10 12:33:10 +0000

    Bread And Soup Discount

--- src/main/java/com/henrys/store/domain/Basket.java
+++ src/main/java/com/henrys/store/domain/Basket.java
@@ -1,24 +1,36 @@
 package com.henrys.store.domain;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Basket {
 
-    private List<Item> items  = new ArrayList<>();
-    private BigDecimal totalPrice;
+    private List<Item> items = new ArrayList<>();
+    private List<Offer> offers = Arrays.asList(new AppleDiscount(), new BreadAndSoupDiscount());
 
     public void addItems(List<String> itemNames) {
-      List<Item> newItems = itemNames.stream().map(Item::getItemByName).collect(Collectors.toList());
-      items.addAll(newItems);
+        List<Item> newItems = itemNames.stream().map(Item::getItemByName).collect(Collectors.toList());
+        items.addAll(newItems);
     }
 
-    public List<Item> listItems(){
+    public List<Item> listItems() {
         return Collections.unmodifiableList(items);
     }
 
+    public BigDecimal getPriceBeforeDiscount() {
+        return items.stream().map(item -> item.price).reduce(BigDecimal.ZERO, BigDecimal::add).setScale(2, RoundingMode.DOWN);
+    }
 
+    public BigDecimal getDiscountedPrice(LocalDate purchaseDate) {
+        BigDecimal totalDiscount =
+                offers.stream().map(offer -> offer.calculateDiscount(items, purchaseDate))
+                        .reduce(BigDecimal.ZERO, BigDecimal::add);
+        return getPriceBeforeDiscount().subtract(totalDiscount).setScale(2,RoundingMode.DOWN);
+    }
 }
--- src/test/java/com/henrys/store/domain/BasketTest.java
+++ src/test/java/com/henrys/store/domain/BasketTest.java
@@ -3,6 +3,8 @@ package com.henrys.store.domain;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.Arrays;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -12,18 +14,25 @@ public class BasketTest {
     private Basket basket;
 
     @Before
-    public void setUp(){
+    public void setUp() {
         basket = new Basket();
     }
 
     @Test
-    public void addValidItems(){
-        basket.addItems(Arrays.asList("apple","apple", "milk", "bread", "soup"));
+    public void addValidItems() {
+        basket.addItems(Arrays.asList("apple", "apple", "milk", "bread", "soup"));
         assertThat(basket.listItems().size()).isEqualTo(5);
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void addInvalidItems(){
-        basket.addItems(Arrays.asList("apple","apple", "milk", "beard", "soup"));
+    public void addInvalidItems() {
+        basket.addItems(Arrays.asList("apple", "apple", "milk", "beard", "soup"));
+    }
+
+    @Test
+    public void getPriceBeforeDiscountApples() {
+        basket.addItems(Arrays.asList("apple", "apple", "milk", "bread", "soup", "soup"));
+        assertThat(basket.getPriceBeforeDiscount()).isEqualTo(new BigDecimal("3.60"));
+        assertThat(basket.getDiscountedPrice(LocalDate.now().plusDays(3))).isEqualTo(new BigDecimal("3.18"));
     }
 }
